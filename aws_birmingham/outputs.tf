fcab333884906a6ffab20329eadcc625817f817f
thilanka <trajaka@gmail.com>
2025-03-22 19:09:56 +0530

    .

--- aws_birmingham/outputs.tf
+++ aws_birmingham/outputs.tf
@@ -1,4 +1,4 @@
-output "sagemaker_endpoint" {
-  value = aws_sagemaker_endpoint.xgboost_endpoint.name
-  description = "SageMaker endpoint for predictions"
+output "sagemaker_model" {
+  value = aws_sagemaker_model.xgboost_model.name
+  description = "SageMaker model name"
 }
